8dbf823348618b1eb3ee037fe1e25f7dc544a0f6
remi <delfosseremi@hotmail.com>
2020-04-28 21:17:58 +0200

    first commit

---
+++ asset/js/app.js
@@ -0,0 +1,8 @@
+$(function(){ 
+   new Typed('#typed', {
+        strings: ["HTML5", "CSS", "BOOTSTRAP", "JAVASCRIPT", "PHP", "SYMFONY", "REACT"],
+        typeSpeed: 100,
+        backDelay: 1500,
+        loop: true,
+    });
+});
